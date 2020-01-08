37405a98893d6fa1077d5b461d2d26ce85d2814a
Marlon Agno <mdj.agno@gmail.com>
2020-01-08 01:43:21 -0500

    made field values for project display functions

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -23,9 +23,9 @@ class Bio extends React.Component {
 
   renderLogos = () => {
     const logos = [ reactIcon, chaiIcon, cssIcon, htmlIcon, javascriptIcon, jestIcon, mochaIcon, nodejsIcon, postgresIcon];
-    return logos.map(icon => {
+    return logos.map((icon, index) => {
       return (
-        <img className="stack-logos" src={icon} alt={`${icon.replace('Icon','')} logo`}/>
+        <img key={index} className="stack-logos" src={icon} alt={`${icon.replace('Icon','')} logo`}/>
       )
     })
   }
@@ -40,7 +40,7 @@ class Bio extends React.Component {
       >
       <div id="about-me" style={this.props.windowWidth > 1024 ? this.props.margin : null}>
         <h3 className="section-header">ABOUT ME</h3>
-        <p>My name is Marlon Agno and I am a full stack web developer. As a scientist and engineer, I aspire to cultivate a genuine curiosity of the world around me.  After graduating with a degree in engineering and working as a research engineer, I still saw a big gap in my personal understanding of one of the pillar technologies that the world is built on today, software. I chose to become a full-stack developer to achieve a deeper understanding of technology, as well as be in a field that is conducive to challenging myself and continually learning. </p>
+        <p>My name is Marlon Agno and I am a full stack web developer. As a scientist and engineer, I aspire to cultivate a genuine curiosity of the world around me.  After graduating with a degree in engineering and working as a research engineer, I found myself looking to learn more about one of the pillar technologies that the world is built on today, software. I chose to become a full-stack developer to achieve a deeper understanding of technology, as well as be in a field that is conducive to challenging myself and continually learning. </p>
         <div className="logos">
           {this.renderLogos()}
         </div>
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,13 +6,20 @@ import { CSSTransition } from 'react-transition-group';
 import nowPlayingOne from '../../app-screenshots/now-playing-1.PNG';
 import nowPlayingTwo from '../../app-screenshots/now-playing-2.PNG';
 import nowPlayingThree from '../../app-screenshots/now-playing-3.PNG';
+import pokerOne from '../../app-screenshots/poker-winrate-1.PNG';
+import pokerTwo from '../../app-screenshots/poker-winrate-2.PNG';
+import pokerThree from '../../app-screenshots/poker-winrate-3.PNG';
+
 
 export default class Portfolio extends React.Component {
   state = {
     appear: false,
-    pokerWinRateMain: nowPlayingOne,
+    pokerWinRateMain: pokerOne,
     spacedRepetitionMain: "",
     nowPlayingMain: nowPlayingOne,
+    nowPlayingMainCaption: "",
+    pokerWinRateMainCaption: "",
+    spacedRepetitionMainCaption: "",
   }
 
   componentDidMount() {
@@ -35,23 +42,43 @@ export default class Portfolio extends React.Component {
     )
   }
   
-  handleImageClick = e => {
-    this.setState({nowPlayingMain: e.target.src})
+  handleImageClick = (field, e) => {
+    const caption = field + 'Caption'
+    this.setState({
+      [field]: e.target.src,
+      [caption]: e.target.alt
+    })
+    console.log(caption)
   }
 
   renderNowPlayingCarousel = () => {
     return (
       <div className="image-carousel">
         <div className="thumbnail-images">
-          <img name="nowPlayingOne" className="thumbnail-image" src={nowPlayingOne} alt="Screen shot of Now Playing home page" onClick={e => this.handleImageClick(e)}/>
-          <img name="nowPlayingTwo" className="thumbnail-image" src={nowPlayingTwo} alt="Screen shot of comment page on Now Playing" onClick={e => this.handleImageClick(e)} />
-          <img name="nowPlayingThree" className="thumbnail-image" src={nowPlayingThree} alt="Screen shot of comment page on Now Playing" onClick={e => this.handleImageClick(e)} />
+          <img name="nowPlayingOne" className="thumbnail-image" src={nowPlayingOne} alt="Screen shot of Now Playing home page" onClick={e => this.handleImageClick('nowPlayingMain', e)}/>
+          <img name="nowPlayingTwo" className="thumbnail-image" src={nowPlayingTwo} alt="Screen shot of comment page on Now Playing" onClick={e => this.handleImageClick('nowPlayingMain', e)} />
+          <img name="nowPlayingThree" className="thumbnail-image" src={nowPlayingThree} alt="Screen shot of comment page on Now Playing" onClick={e => this.handleImageClick('nowPlayingMain', e)} />
         </div>
-        <img name="nowPlayingOne" className="main-image" src={this.state.nowPlayingMain} alt="Screen shot of Now Playing home page"/>
+        <img name="nowPlayingMain" className="main-image" src={this.state.nowPlayingMain} alt="Screen shot of Now Playing home page"/>
       </div>
 
     )
   }
+
+  renderPokerWinrateCarousel = () => {
+    return (
+      <div className="image-carousel">
+        <div className="thumbnail-images">
+          <img name="pokerOne" className="thumbnail-image" src={pokerOne} alt="Screen shot of Now Playing home page" onClick={e => this.handleImageClick('pokerWinRateMain', e)}/>
+          <img name="pokerTwo" className="thumbnail-image" src={pokerTwo} alt="Screen shot of comment page on Now Playing" onClick={e => this.handleImageClick('pokerWinRateMain', e)} />
+          <img name="pokerThree" className="thumbnail-image" src={pokerThree} alt="Screen shot of comment page on Now Playing" onClick={e => this.handleImageClick('pokerWinRateMain', e)} />
+        </div>
+        <img name="pokerWinRateMain" className="main-image" src={this.state.pokerWinRateMain} alt="Screen shot of Now Playing home page"/>
+      </div>
+
+    )
+  }
+
 
   render() {
     return (
@@ -70,7 +97,8 @@ export default class Portfolio extends React.Component {
               <p className="project-description">
               Poker WinRate is an interactive web application that allows users to track results after each poker session to determine their win rate over time.  As much as poker is a game of skill, luck is a huge element of the game and it is important to track results to determine a player’s edge over a long period of time.  This application allows users to input their sessions indicating the game type, buy-in amount, and how much they cashed out.  The results are then compiled and sorted, and users are provided a data visualization in the analytics section.
               </p>
-              {this.renderNowPlayingCarousel()}
+              {this.renderPokerWinrateCarousel()}
+              <p className="main-image-caption">{this.state.pokerWinRateMainCaption}</p>
             </Element>
             {this.renderProjectButtons("", "https://github.com/thinkful-ei-gecko/marlon-poker-winrate-client", "https://github.com/thinkful-ei-gecko/marlon-poker-winrate-server")}
           </li>
@@ -84,6 +112,7 @@ export default class Portfolio extends React.Component {
               To simplify thread creation, a search engine was implemented using the TMDB API to pull data for existing movies and populate information for created threads including the time, poster, and trailer.  A live update of ongoing “happenings” is also featured on the home page of the application.
               </p>
               {this.renderNowPlayingCarousel()}
+              <p className="main-image-caption">{this.state.nowPlayingMainCaption}</p>
             </Element>  
             {this.renderProjectButtons("https://playing-phi-ten.now.sh/", "https://github.com/thinkful-ei-gecko/NowPlaying-Client", "https://github.com/thinkful-ei-gecko/NowPlaying-Server")}
           </li>
@@ -96,6 +125,7 @@ export default class Portfolio extends React.Component {
               By implementing an algorithm on the server to correctly space cards that a user guesses correctly and incorrectly, the Spaced Repetition application can help a user memorize a language more effectively.  Currently, the supported language in Spaced Repetition is Morse.
               </p>
               {this.renderNowPlayingCarousel()}
+              <p className="main-image-caption">{this.state.spacedRepetitionMainCaption}</p>
             </Element>
             {this.renderProjectButtons("", "https://github.com/thinkful-ei-gecko/shawn-marlon-spaced-rep", "https://github.com/thinkful-ei-gecko/shawn-marlon-spaced-rep-api")}
           </li>

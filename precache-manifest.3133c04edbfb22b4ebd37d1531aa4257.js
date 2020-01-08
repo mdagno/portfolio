2ffb29f86d6abaad16b750f9b0a2a80aa1645b88
Marlon Agno <mdj.agno@gmail.com>
2020-01-08 04:11:46 -0500

    Updates

---
+++ precache-manifest.3133c04edbfb22b4ebd37d1531aa4257.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c72459b5a899b6202b7eb8277f78d98",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3757673657eb2499efed",
+    "url": "/portfolio/static/css/main.c97817d7.chunk.css"
+  },
+  {
+    "revision": "33844cf4ecc800cea0d0",
+    "url": "/portfolio/static/js/2.6cd389d4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio/static/js/2.6cd389d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3757673657eb2499efed",
+    "url": "/portfolio/static/js/main.9d378cea.chunk.js"
+  },
+  {
+    "revision": "689ce7b0d8724cc22499",
+    "url": "/portfolio/static/js/runtime-main.32894e83.js"
+  },
+  {
+    "revision": "c88d5e0b9e0e89b2dcd55a000df96c0c",
+    "url": "/portfolio/static/media/Marlon Agno Resume.c88d5e0b.pdf"
+  },
+  {
+    "revision": "16809aa7430829676bea374431cd5308",
+    "url": "/portfolio/static/media/chai.16809aa7.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/portfolio/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "9a30e6c6c75344e5bd217bbe9720ef3e",
+    "url": "/portfolio/static/media/footer_lodyas.9a30e6c6.png"
+  },
+  {
+    "revision": "e67c6af8237338b75bc64b81a1c7d096",
+    "url": "/portfolio/static/media/gmail-icon.e67c6af8.svg"
+  },
+  {
+    "revision": "f0361c5e9bebc1c5647523db385641cf",
+    "url": "/portfolio/static/media/html5.f0361c5e.svg"
+  },
+  {
+    "revision": "61ed8b054178d0e8d1285ad8f29f4045",
+    "url": "/portfolio/static/media/javascript.61ed8b05.svg"
+  },
+  {
+    "revision": "ea9cdc83e9e0ebe92e5583f9ff466b5c",
+    "url": "/portfolio/static/media/jest-0.ea9cdc83.svg"
+  },
+  {
+    "revision": "da41a70434d3facd7e4c9728f28ef141",
+    "url": "/portfolio/static/media/jquery-1.da41a704.svg"
+  },
+  {
+    "revision": "4f2bc978122ea69dc7033f2f60cb0fe5",
+    "url": "/portfolio/static/media/linkedin-icon-2.4f2bc978.svg"
+  },
+  {
+    "revision": "ae200940324d4cadc039f6687e94f68e",
+    "url": "/portfolio/static/media/mocha-1.ae200940.svg"
+  },
+  {
+    "revision": "b337a2fb0b1289e6757e632dc766ff6b",
+    "url": "/portfolio/static/media/nodejs-icon.b337a2fb.svg"
+  },
+  {
+    "revision": "9951f14720c36062c0f1ca3faed4dc48",
+    "url": "/portfolio/static/media/now-playing-1.9951f147.PNG"
+  },
+  {
+    "revision": "44c1df3a7a082225a61e234867aebc51",
+    "url": "/portfolio/static/media/now-playing-2.44c1df3a.PNG"
+  },
+  {
+    "revision": "4725da7e5bea8d3705005881d9461e92",
+    "url": "/portfolio/static/media/now-playing-3.4725da7e.PNG"
+  },
+  {
+    "revision": "508066ab3c60e3a4d05f9fe8da56487c",
+    "url": "/portfolio/static/media/poker-winrate-1.508066ab.PNG"
+  },
+  {
+    "revision": "6141c7a31418a2189de38d6143046416",
+    "url": "/portfolio/static/media/poker-winrate-2.6141c7a3.PNG"
+  },
+  {
+    "revision": "81431db23769887f5bdc95b6dbd63b02",
+    "url": "/portfolio/static/media/poker-winrate-3.81431db2.PNG"
+  },
+  {
+    "revision": "d7c985e7cca077dc2d77f89a66573e35",
+    "url": "/portfolio/static/media/postgresql.d7c985e7.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/portfolio/static/media/react.4009eef3.svg"
+  }
+]);

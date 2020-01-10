0acc276a9a9261daf0951c54bd537e7c326dd805
Marlon Agno <mdj.agno@gmail.com>
2020-01-09 20:25:15 -0500

    Updates

---
+++ precache-manifest.7227e9fbe11e97da2b799edcba23d052.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a7249e966e6696291dbfe1943013e4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "48a91363387995ce1333",
+    "url": "/portfolio/static/css/main.74b5da66.chunk.css"
+  },
+  {
+    "revision": "cff22b0deb02b2dab700",
+    "url": "/portfolio/static/js/2.2f3324e0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/portfolio/static/js/2.2f3324e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "48a91363387995ce1333",
+    "url": "/portfolio/static/js/main.3a3c9803.chunk.js"
+  },
+  {
+    "revision": "689ce7b0d8724cc22499",
+    "url": "/portfolio/static/js/runtime-main.32894e83.js"
+  },
+  {
+    "revision": "c88d5e0b9e0e89b2dcd55a000df96c0c",
+    "url": "/portfolio/static/media/Marlon Agno Resume.c88d5e0b.pdf"
+  },
+  {
+    "revision": "16809aa7430829676bea374431cd5308",
+    "url": "/portfolio/static/media/chai.16809aa7.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/portfolio/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "9a30e6c6c75344e5bd217bbe9720ef3e",
+    "url": "/portfolio/static/media/footer_lodyas.9a30e6c6.png"
+  },
+  {
+    "revision": "e67c6af8237338b75bc64b81a1c7d096",
+    "url": "/portfolio/static/media/gmail-icon.e67c6af8.svg"
+  },
+  {
+    "revision": "f0361c5e9bebc1c5647523db385641cf",
+    "url": "/portfolio/static/media/html5.f0361c5e.svg"
+  },
+  {
+    "revision": "61ed8b054178d0e8d1285ad8f29f4045",
+    "url": "/portfolio/static/media/javascript.61ed8b05.svg"
+  },
+  {
+    "revision": "ea9cdc83e9e0ebe92e5583f9ff466b5c",
+    "url": "/portfolio/static/media/jest-0.ea9cdc83.svg"
+  },
+  {
+    "revision": "da41a70434d3facd7e4c9728f28ef141",
+    "url": "/portfolio/static/media/jquery-1.da41a704.svg"
+  },
+  {
+    "revision": "4f2bc978122ea69dc7033f2f60cb0fe5",
+    "url": "/portfolio/static/media/linkedin-icon-2.4f2bc978.svg"
+  },
+  {
+    "revision": "ae200940324d4cadc039f6687e94f68e",
+    "url": "/portfolio/static/media/mocha-1.ae200940.svg"
+  },
+  {
+    "revision": "b337a2fb0b1289e6757e632dc766ff6b",
+    "url": "/portfolio/static/media/nodejs-icon.b337a2fb.svg"
+  },
+  {
+    "revision": "9951f14720c36062c0f1ca3faed4dc48",
+    "url": "/portfolio/static/media/now-playing-1.9951f147.PNG"
+  },
+  {
+    "revision": "44c1df3a7a082225a61e234867aebc51",
+    "url": "/portfolio/static/media/now-playing-2.44c1df3a.PNG"
+  },
+  {
+    "revision": "4725da7e5bea8d3705005881d9461e92",
+    "url": "/portfolio/static/media/now-playing-3.4725da7e.PNG"
+  },
+  {
+    "revision": "508066ab3c60e3a4d05f9fe8da56487c",
+    "url": "/portfolio/static/media/poker-winrate-1.508066ab.PNG"
+  },
+  {
+    "revision": "6141c7a31418a2189de38d6143046416",
+    "url": "/portfolio/static/media/poker-winrate-2.6141c7a3.PNG"
+  },
+  {
+    "revision": "81431db23769887f5bdc95b6dbd63b02",
+    "url": "/portfolio/static/media/poker-winrate-3.81431db2.PNG"
+  },
+  {
+    "revision": "d7c985e7cca077dc2d77f89a66573e35",
+    "url": "/portfolio/static/media/postgresql.d7c985e7.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/portfolio/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "115e0f7d6d47b551c6782b67c42daa35",
+    "url": "/portfolio/static/media/sr-one.115e0f7d.PNG"
+  },
+  {
+    "revision": "b79605076b388130940cb5943b6c9d7d",
+    "url": "/portfolio/static/media/sr-three.b7960507.PNG"
+  },
+  {
+    "revision": "ee951b59d1ac978cde7393ef04389006",
+    "url": "/portfolio/static/media/sr-two.ee951b59.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a403dfd793ac656ade41b765398d8dce.js"
+  "/portfolio/precache-manifest.7227e9fbe11e97da2b799edcba23d052.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
